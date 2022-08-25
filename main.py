0e9f4cd1ae8d85270e71980d6b79f090db967c26
brianb12321 <brian.jx.b@gmail.com>
2022-08-25 18:41:45 -0500

    Moved main code into main function

--- main.py
+++ main.py
@@ -3,29 +3,36 @@ from collections import defaultdict
 from segment import SegmentType, create_segment_from_input
 import segment_calculations as calc
 
-if __name__ == "__main__":
+def read_from_stdin_and_create_segments():
+    segments = defaultdict(list)
+    first_segment = None
+    last_segment = None
+
+    while True:
+        print("> ", end="")
+        input_text = input()
+
+        if not input_text:
+            break
+
+        new_segment = create_segment_from_input(input_text)
+        segments[new_segment.type].append(new_segment)
+
+        if first_segment is None:
+            first_segment = new_segment
+
+        last_segment = new_segment
+    
+    return (segments, first_segment, last_segment)
+
+def main():
     os.system("cls")
     print("Welcome to engineering units QC")
 
     while True:
-        segments = defaultdict(list)
-        first_segment = None
         totals = {}
 
-        while True:
-            print("> ", end="")
-            input_text = input()
-
-            if not input_text:
-                break
-
-            new_segment = create_segment_from_input(input_text)
-            segments[new_segment.type].append(new_segment)
-
-            if first_segment is None:
-                first_segment = new_segment
-
-            last_segment = new_segment
+        segments, first_segment, last_segment = read_from_stdin_and_create_segments()
         
         totals[SegmentType.PLOW] = calc.calculate_segments_plow(segments[SegmentType.PLOW], first_segment, last_segment)
         totals[SegmentType.BORE] = calc.calculate_segments_bore(segments[SegmentType.BORE], first_segment, last_segment)
@@ -36,3 +43,6 @@ if __name__ == "__main__":
 
         print("Press ENTER when ready.")
         input()
+
+if __name__ == "__main__":
+    main()
